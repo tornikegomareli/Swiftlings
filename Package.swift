74476610f34ddc0489673cf0c129c462bebcaada
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-02-12 23:55:37 +0400

    initial package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Swiftlings",
+  platforms: [
+    .macOS(.v13)
+  ],
+  products: [
+    .library(
+      name: "Swiftlings",
+      targets: ["Exercises"]),
+  ],
+  targets: [
+    .target(
+      name: "Exercises"),
+    .testTarget(
+      name: "TestsForExercises",
+      dependencies: ["Exercises"]
+    ),
+  ]
+)
